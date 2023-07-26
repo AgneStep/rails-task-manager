b6d520f883ed98829571bcceb3922803e89c3501
agne steponaviciute <agne.step@outlook.com>
2023-07-26 13:19:43 +0100

    task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,6 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-
   def new
     @task = Task.new
   end
@@ -15,5 +14,27 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
